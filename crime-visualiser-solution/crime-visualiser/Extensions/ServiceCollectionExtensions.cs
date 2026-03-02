b7d1d7fbc4b95cb4c83da0816af59416789e39a1
Harry Carter <harry.carter900@gmail.com>
2026-03-02 12:15:46 +0000

    Version 1 Release
    * wip
    
    * wip
    
    * update
    
    * wip
    
    * Project v1 release

---
+++ crime-visualiser-solution/crime-visualiser/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using crime_visualiser.Constants;
+using crime_visualiser.HttpClients;
+using crime_visualiser.HttpClients.Interfaces;
+using System.Security.Authentication;
+
+namespace crime_visualiser.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
+    {
+        var baseUrl = configuration.GetValue<string>($"ApiBaseUrls:{HttpClientNames.CrimeDataApi}");
+        if (baseUrl == null ) {
+            throw new InvalidOperationException($"Base URL for {HttpClientNames.CrimeDataApi} is not configured.");
+        }
+
+        services.AddHttpClient<IUkCrimeDataClient, UkCrimeDataClient>(client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+            client.Timeout = TimeSpan.FromSeconds(30);
+        })
+        .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+        {
+            SslOptions = { EnabledSslProtocols = SslProtocols.Tls13 },
+        });
+
+        return services;
+    }
+}
---
+++ crime-visualiser-solution/crime-visualiser/Services/Interfaces/ICrimeDataService.cs
@@ -0,0 +1,8 @@
+﻿using crime_visualiser.Models;
+
+namespace crime_visualiser.Services.Interfaces;
+
+public interface ICrimeDataService
+{
+    public Task<IEnumerable<CrimeCategoryCount>?> GetCrimeCategoryCountsAsync(CrimeQuery crimeQuery);
+}
